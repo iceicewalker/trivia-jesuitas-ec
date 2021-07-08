b51ff623b793d2061e1afecbfe0693616edf14f9
iceicewalker <dabarreno@hotmail.com>
2021-07-07 21:01:24 -0500

    play component

--- src/app/play/play.page.ts
+++ src/app/play/play.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-play',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
+  goHome(){
+    this.resetGame();
+    this.router.navigate(['/home']);
+  }
+
+  resetGame(){
+    
+  }
 }
