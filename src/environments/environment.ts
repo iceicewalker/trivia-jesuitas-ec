1ee50cc7b1339930329312e6fd171a44d4ca2425
iceicewalker <dabarreno@hotmail.com>
2021-07-22 09:48:04 -0500

    Logo Puce

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyBJA2c2jTmxAaRh9aeK34ZZjv8vPBLcsEs",
     authDomain: "trivia-jesuitas-ec.firebaseapp.com",
